9b59c166be4e057a99d90871dbdc3f8a3bf2781b
Jeremy Duc <nashoba.jeremy@gmail.com>
2025-05-20 16:42:00 +0200

    changes

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,10 +2,10 @@ export default defineNuxtRouteMiddleware(async () => {
     const token = useCookie('token')
     const { user, getUser } = useAuth()
 
-    if (!token.value) return navigateTo('/login')
+    if (!token.value) return navigateTo('/auth/login')
 
     if (!user.value) {
         const u = await getUser()
-        if (!u) return navigateTo('/login')
+        if (!u) return navigateTo('/auth/login')
     }
 })
